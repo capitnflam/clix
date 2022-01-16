05916eec0cb685eac7f76a37fe1c9bf285eee3f9
tony-go <gorez.tony@gmail.com>
2022-01-16 17:32:38 +0100

    chore: update test.mjs

--- test.mjs
+++ test.mjs
@@ -1,3 +1,8 @@
 import { clix } from './index.js'
 
+const scenario = clix('super 2')
+  .expect(['Santa', 'New Year'])
+  .input()
+  .expectError('Failed')
+
 console.log('From native', clix('ls -la'));
